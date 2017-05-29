2c65ac4ecdc50c5610105f5998e38badeee230d0
Kevin de Leeuw <lion.drie@gmail.com>
2017-05-29 20:57:52 +0200

    builder pattern

---
+++ Builder/src/builder/PizzaBuilderDemo.java
@@ -0,0 +1,132 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package builder;
+
+/* "Product" */
+class Pizza {
+    private String dough = "";
+    private String sauce = "";
+    private String topping = "";
+    private String pizzaType = "";
+
+    public void setDough(String dough) {
+        this.dough = dough;
+    }
+
+    public void setSauce(String sauce) {
+        this.sauce = sauce;
+    }
+
+    public void setTopping(String topping) {
+        this.topping = topping;
+    }
+    public void setPizzaType(String pizzaType){
+        this.pizzaType = pizzaType;
+    }
+    
+    public String getPizzaType(){
+        return pizzaType;
+    }
+}
+
+/* "Abstract Builder" */
+abstract class PizzaBuilder {
+    protected Pizza pizza;
+
+    public Pizza getPizza() {
+        return pizza;
+    }
+
+    public void createNewPizzaProduct() {
+        pizza = new Pizza();
+    }
+
+    public abstract void buildDough();
+    public abstract void buildSauce();
+    public abstract void buildTopping();
+    public abstract void declarePizza();
+}
+
+/* "ConcreteBuilder" */
+class HawaiianPizzaBuilder extends PizzaBuilder {
+    public void buildDough() {
+        pizza.setDough("cross");
+    }
+
+    public void buildSauce() {
+        pizza.setSauce("mild");
+    }
+
+    public void buildTopping() {
+        pizza.setTopping("ham+pineapple");
+    }
+    
+    public void declarePizza(){
+        pizza.setPizzaType("Hawai");
+    }
+   
+}
+
+/* "ConcreteBuilder" */
+class SpicyPizzaBuilder extends PizzaBuilder {
+    public void buildDough() {
+        pizza.setDough("pan baked");
+    }
+
+    public void buildSauce() {
+        pizza.setSauce("hot");
+    }
+
+    public void buildTopping() {
+        pizza.setTopping("pepperoni+salami");
+    }
+     public void declarePizza(){
+        pizza.setPizzaType("Spicy");
+    }
+}
+
+/* "Director" */
+class Waiter {
+    private PizzaBuilder pizzaBuilder;
+
+    public void setPizzaBuilder(PizzaBuilder pb) {
+        pizzaBuilder = pb;
+    }
+
+    public Pizza getPizza() {
+        return pizzaBuilder.getPizza();
+    }
+
+    public void constructPizza() {
+        pizzaBuilder.createNewPizzaProduct();
+        pizzaBuilder.buildDough();
+        pizzaBuilder.buildSauce();
+        pizzaBuilder.buildTopping();
+        pizzaBuilder.declarePizza();
+    }
+}
+
+/* A customer ordering a pizza. */
+public class PizzaBuilderDemo {
+    public static void main(String[] args) {
+        Waiter waiter = new Waiter();
+        PizzaBuilder hawaiianPizzabuilder = new HawaiianPizzaBuilder();
+        PizzaBuilder spicyPizzaBuilder = new SpicyPizzaBuilder();
+
+        waiter.setPizzaBuilder( hawaiianPizzabuilder );
+         waiter.constructPizza();
+         
+        Pizza pizza = waiter.getPizza();
+         System.out.println(pizza.getPizzaType());
+
+       
+       
+       
+     
+    }
+}
+
